71a5661d578cd871e6d9d796a183e70f65205ac3
Nyein Chan Aung <63293974+NyeinChanAung203@users.noreply.github.com>
2024-05-06 17:00:30 +0630

    filter with Q

--- store/views.py
+++ store/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Product
 
 
 def hello(request):
-    products = Product.objects.filter(last_update__year__lt=2023);
+    products = Product.objects.filter(
+        Q(inventory__lt=10) | Q(unit_price=20)
+    );
+    products = Product.objects.filter(
+        Q(inventory__lt=10) & Q(unit_price=20)
+    );
+    products = Product.objects.filter(
+        Q(inventory__lt=10) & ~Q(unit_price=20)
+    );
+    # ~ not
     return render(request,'hello.html',{'products': list(products)})
