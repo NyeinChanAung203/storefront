34f90688674c03fc4e6816f700d3358a703f8353
Nyein Chan Aung <63293974+NyeinChanAung203@users.noreply.github.com>
2024-05-06 16:58:12 +0630

    filter orm

--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def hello(request):
+    products = Product.objects.filter(last_update__year__lt=2023);
+    return render(request,'hello.html',{'products': list(products)})
